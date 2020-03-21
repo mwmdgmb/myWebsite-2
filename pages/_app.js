cdfaf06a509dc3965998e73d5c206be531cd17ab
mwmdgmb <sadboy7m@gmail.com>
2020-03-21 18:50:41 +0430

    create-portfolio

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import '../assets/styleList/main.scss'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
